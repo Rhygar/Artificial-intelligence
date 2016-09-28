b38106a87ccc87c888c8bc2295381cac51adc9a7
taurus84 <David@helengrvallsmbp.clients.internal.mah.se>
2016-09-28 15:45:38 +0200

    class State commented. DONE

--- src/State.java
+++ src/State.java
@@ -1,7 +1,9 @@
 /**
- * This class contains 
- * @author David
- *
+ * This class contains the current state of the game. 
+ * It consists of a board represented by a two dimensional int array
+ * where -1 represents a human piece and +1 represents computer piece. 0 means empty square
+ * @author David Tran & John Tengvall
+ * date: 28-09-2016
  */
 
 public class State {
@@ -15,21 +17,43 @@ public class State {
 							 {0,0,0,0}
 	};
 	
+	/**
+	 * Constructor with defined board
+	 * @param board
+	 */
 	public State(int[][] board) {
 		this.board = board;
 	}
 	
+	/**
+	 * Constructor with initial board
+	 */
 	public State() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * Method returns the owner of a specific sqaure
+	 * @param row row of the sqaure
+	 * @param col column of the square
+	 * @return The owner. -1 human, +1 com, 0 empty
+	 */
 	public int getOwner(int row,int col){
 		return board[row][col];
 	}
 
+	/**
+	 * Method returns the board
+	 * @return the board as a two dimensional int array
+	 */
 	public int[][] getBoard() {
 		return this.board;
 	}
 	
+	/**
+	 * Method sets a board to the state
+	 * @param board Two dimensional int array which represents the board
+	 */
 	public void setBoard(int[][] board) {
 		this.board = board;
 	}
@@ -52,21 +76,14 @@ public class State {
 		return score;
 	}
 	
+	/**
+	 * Method sets a square to -1, 1 or 0
+	 * @param row row of the square
+	 * @param col column of the square
+	 * @param player Which player, human, computer or empty 
+	 */
 	public void updateBoard(int row, int col,int player ){
 		this.board[row][col] = player;
 		
 	}
-	
-	public State playerMove(State state, int row, int col){
-		return null;
-		
-	}
-	
-	public State comMove(State state, int row, int col){
-		State tempBoard = new State(state.getBoard());
-		return tempBoard;
-	}
-	
-	
-	
 }
